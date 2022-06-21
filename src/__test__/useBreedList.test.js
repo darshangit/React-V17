dd07029b044d7d07ba751994ca0d3029d7c3ffab
Darshan Satya <darshans1989@gmail.com>
2022-06-21 08:39:09 -0400

    test

---
+++ src/__test__/useBreedList.test.js
@@ -0,0 +1,31 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import { expect, test } from "@jest/globals";
+import { renderHook } from "@testing-library/react-hooks";
+import useBreedList from "../useBreedList";
+
+test("gives an empty list with no animal", async () => {
+  const { result } = renderHook(() => useBreedList(""));
+  const [breedList, status] = result.current;
+  expect(breedList).toHaveLength(0);
+  expect(status).toBe("unloaded");
+});
+
+test("gives back breeds with an animal", async () => {
+  const breeds = ["Havenese", "Poodle", "Maltese", "Husky", "Labrador"];
+
+  fetch.mockResponseOnce(
+    JSON.stringify({
+      animal: "dog",
+      breeds,
+    })
+  );
+  const { result, waitForNextUpdate } = renderHook(() => useBreedList("dog"));
+  await waitForNextUpdate();
+
+  const [breedList, status] = result.current;
+  expect(status).toBe("loaded");
+  expect(breedList).toEqual(breeds);
+});
