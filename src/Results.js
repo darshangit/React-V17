c25566142fb564a17bbebe8aa2ecb1d7470c25a1
Darshan Satya <darshans1989@gmail.com>
2022-06-21 08:51:50 -0400

    snapshot testing

--- src/Results.js
+++ src/Results.js
@@ -4,7 +4,7 @@ const Results = ({ pets }) => {
   return (
     <div>
       {!pets.length ? (
-        <h1>No Pets Found</h1>
+        <h1>No Pets Founds</h1>
       ) : (
         pets.map((pet) => (
           <Pet
